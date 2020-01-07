ca53f31ff42aa45bca613212953bd0b561d3f6ba
clementval <clementval@gmail.com>
2020-01-07 09:14:18 -0500

    OpenACC dialect structure draft

---
+++ mlir/include/mlir/Conversion/OpenACC/ConvertOpenACCToSeq.h
@@ -0,0 +1,27 @@
+//===- ConvertOpenACCToSeq.h - Convert OpenACC to Standard ops --*- C++ -*-===//
+//
+// Part of the MLIR Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// =============================================================================
+//
+// Provides patterns to convert from OpenACC structure ops to standard ops.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef MLIR_CONVERSION_OPENACC_CONVERTOPENACCTOSEQ_H
+#define MLIR_CONVERSION_OPENACC_CONVERTOPENACCTOSEQ_H
+
+#include "mlir/Dialect/OpenACC/OpenACC.h"
+#include "mlir/Support/StringExtras.h"
+#include "mlir/Transforms/DialectConversion.h"
+
+namespace mlir {
+    template <typename T> class OpPassBase;
+
+    std::unique_ptr<OpPassBase<FuncOp>> createOpenACCToSeqPass();
+
+} // namespace mlir
+
+#endif // MLIR_CONVERSION_OPENACC_CONVERTOPENACCTOSEQ_H
---
+++ mlir/include/mlir/Dialect/OpenACC/OpenACC.h
@@ -0,0 +1,41 @@
+//===- OpenACC.h - MLIR OpenACC Dialect -------------------------*- C++ -*-===//
+//
+// Part of the MLIR Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+// ============================================================================
+//
+// This file defines the OpenACC related operations and puts them in the
+// corresponding dialect.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef MLIR_DIALECT_OPENACC_DIALECT_H
+#define MLIR_DIALECT_OPENACC_DIALECT_H
+
+#include "mlir/IR/Dialect.h"
+#include "mlir/IR/OpDefinition.h"
+
+namespace mlir {
+    namespace acc {
+
+        class OpenACCDialect : public Dialect {
+        public:
+            explicit OpenACCDialect(MLIRContext *context);
+
+            static StringRef getDialectNamespace() { return "acc"; }
+
+            static StringRef getCollapseAttrName() { return "collapse"; }
+
+            static StringRef getAsyncAttrName() { return "async"; }
+        };
+
+#define GET_OP_CLASSES
+
+#include "mlir/Dialect/OpenACC/OpenACC.h.inc"
+
+    } // end namespace acc
+} // end namespace mlir
+
+#endif // MLIR_DIALECT_OPENACC_DIALECT_H
