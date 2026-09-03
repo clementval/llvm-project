0f1d14d717bd2d1f13df926a173a54837b87b758
Valentin Clement <clementval@gmail.com>
2026-09-03 16:44:37 -0700

    [mlir][openacc] Hanlde compiler generated global for unified mode

--- mlir/lib/Dialect/OpenACC/Transforms/ACCDeclareGPUModuleInsertion.cpp
+++ mlir/lib/Dialect/OpenACC/Transforms/ACCDeclareGPUModuleInsertion.cpp
@@ -115,7 +115,7 @@ public:
           cudaUnified &&
           declareAttr.getDataClause().getValue() !=
               acc::DataClause::acc_declare_device_resident &&
-          (!globalVar || !globalVar.isConstant());
+              (!globalVar || !globalVar.isConstant() || globalVar.isCompilerGenerated());
       if (makeUnifiedDeclaration)
         makeDeviceGlobalDeclaration(*deviceGlobal);
 
